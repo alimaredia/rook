ae336cf7df0d927d354fb14f864851fde015c766
Ali Maredia <amaredia@redhat.com>
2020-06-30 02:24:00 -0400

    ceph: enable the realm pull on CephObjectRealm
    This commit:
    - adds the pull section on the CephObjectRealm spec
    to enables realms to be pulled instead of created.
    
    - creates the system user and generates the access
    key and secret key for the user and zones in a realm.
    
    - adds "omitempty" to fields in multisite CRs that
    are not required
    
    Signed-off-by: Ali Maredia <amaredia@redhat.com>

--- pkg/apis/ceph.rook.io/v1/types.go
+++ pkg/apis/ceph.rook.io/v1/types.go
@@ -406,7 +406,7 @@ type ObjectStoreSpec struct {
 	Gateway GatewaySpec `json:"gateway"`
 
 	// The multisite info
-	Zone ZoneSpec `json:"zone"`
+	Zone ZoneSpec `json:"zone,omitempty"`
 
 	// The rgw endpoint healthcheck
 	HealthCheck BucketHealthCheckSpec `json:"healthCheck"`
@@ -521,6 +521,11 @@ type CephObjectRealmList struct {
 
 // ObjectRealmSpec represent the spec of an ObjectRealm
 type ObjectRealmSpec struct {
+	Pull PullSpec `json:"pull"`
+}
+
+type PullSpec struct {
+	Endpoint string `json:"endpoint"`
 }
 
 // +genclient
